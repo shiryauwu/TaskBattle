2cc573591246092db7aef9a7c3a49ebfa6fe2ae7
Ilnur <Ilnur.Shamazov@bpmsoft.ru>
2025-02-21 11:16:41 +0300

    Добавил шапку профиля

--- task_battle/lib/profile_page.dart
+++ task_battle/lib/profile_page.dart
@@ -11,7 +11,14 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        // leading: BackButton(color: Colors.green, onPressed: () => MainNavigationBar.new()), // const Text('Назад', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500)),
+        title: const Text('Профиль', style: TextStyle(color: Colors.green, fontSize: 30, fontWeight: FontWeight.w600 )),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
       
     );
     

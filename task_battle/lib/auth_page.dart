249eb98be8935f816ba3fddab7ca38876a23bd25
amir <krasnov.amir1236@gmail.com>
2025-03-15 02:09:27 +0300

    commit with complete authorization and registration

--- task_battle/lib/auth_page.dart
+++ task_battle/lib/auth_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -16,48 +15,48 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
   bool isLogin = true;
 
   Future<void> _authenticate() async {
     try {
       final url = Uri.parse('http://46.29.161.173:80/api/v1/User/${isLogin ? 'authorize' : 'register'}');
-      print(usernameController.text);
-      print(passwordController.text);
-      print(url);
       final response = await http.post(
         url,
         headers: {'Content-Type': 'application/json'},
         body: json.encode({
-          'username': usernameController.text,
+          'username' : usernameController.text,
+          !isLogin?'email': '': emailController.text,
           'password': passwordController.text,
         }),
       );
 
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-
-        // Создаём объект User из ответа сервера
         User user = User.fromJson(data);
 
-        // Сохраняем данные пользователя в SharedPreferences
         final prefs = await SharedPreferences.getInstance();
         await prefs.setString('auth_token', user.token.toString());
         await prefs.setString('username', user.username.toString());
         await prefs.setString('id', user.id.toString());
 
-        // Переход к главному экрану
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => const MainNavigationBar()),
+        );
+      } else if (response.statusCode == 400 && !isLogin) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Ошибка: Некорректный формат email')),
         );
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Ошибка: ${response.body}')),
         );
       }
-      } catch (e) {
+    } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('$e')),
+        SnackBar(content: Text('Ошибка: $e')),
       );
     }
   }
@@ -80,11 +79,28 @@ class _AuthScreenState extends State<AuthScreen> {
               decoration: const InputDecoration(labelText: 'Логин'),
             ),
             const SizedBox(height: 8),
+            if (!isLogin)
+              const SizedBox(height: 8),
+            if (!isLogin)
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                obscureText: false,
+              ),
             TextField(
               controller: passwordController,
               decoration: const InputDecoration(labelText: 'Пароль'),
               obscureText: true,
             ),
+            if (!isLogin)
+              const SizedBox(height: 8),
+            if (!isLogin)
+              TextField(
+                controller: confirmPasswordController,
+                decoration: const InputDecoration(labelText: 'Повторите пароль'),
+                obscureText: true,
+              ),
+             
             const SizedBox(height: 16),
             ElevatedButton(
               onPressed: _authenticate,
@@ -94,6 +110,9 @@ class _AuthScreenState extends State<AuthScreen> {
               onPressed: () {
                 setState(() {
                   isLogin = !isLogin;
+                  usernameController.clear();
+                  passwordController.clear();
+                  confirmPasswordController.clear();
                 });
               },
               child: Text(isLogin ? 'Нет аккаунта? Регистрация' : 'Уже есть аккаунт? Войти'),
